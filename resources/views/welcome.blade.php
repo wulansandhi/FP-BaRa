d66081eb5301be5997fc511feaebae4cb46c1e16
pukiskun <fatalitycloud@gmail.com>
2023-07-22 09:27:27 +0700

    bikin landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-    <!-- Your specific page content goes here -->
-    <!-- For example: -->
-    <div class="container mt-4">
-        <h1>Welcome to the website!</h1>
-        <!-- Add more content as needed -->
-    </div>
+    <!-- Main content -->
+    <main class="container my-4">
+
+        <!-- News Cards -->
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <!-- Sample News Card 1 -->
+            <div class="col">
+                <div class="card">
+                    <img src="https://via.placeholder.com/400" class="card-img-top" alt="News Image">
+                    <div class="card-body">
+                        <h5 class="card-title">News Headline 1</h5>
+                        <p class="card-text">A short description of the news article.</p>
+                        <a href="#" class="btn btn-primary">Read More</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Sample News Card 2 -->
+            <div class="col">
+                <div class="card">
+                    <img src="https://via.placeholder.com/400" class="card-img-top" alt="News Image">
+                    <div class="card-body">
+                        <h5 class="card-title">News Headline 2</h5>
+                        <p class="card-text">A short description of the news article.</p>
+                        <a href="#" class="btn btn-primary">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- Footer -->
+    <footer class="bg-dark text-white text-center py-3">
+        <p>&copy; 2023 News Website. All rights reserved.</p>
+    </footer>
 @endsection
